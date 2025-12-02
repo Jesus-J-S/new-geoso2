232b8115b035ee1c7859b222c0657a7e590e4cce
fp25ifcm0182 <fp25ifcm0182@aulatomillo.org>
2025-12-02 15:43:20 +0100

    navbar y carrusel terminados usando jinja2

--- geoso2-web-template/modify_template.py
+++ geoso2-web-template/modify_template.py
@@ -3,6 +3,7 @@ import json
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 import os
+import shutil
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,12 +11,8 @@ def main():
     output_dir = os.path.join(base_dir, "output")
     os.makedirs(output_dir, exist_ok=True)
 
-    print("Usando templates dir:", templates_dir)
-
-    # Inicializar Jinja2 con la ruta correcta
     env = Environment(loader=FileSystemLoader(templates_dir))
 
-    # --- Carrusel ---
     imagenes = [
         "img/foto1.jpg",
         "img/foto2.png",
@@ -24,11 +21,30 @@ def main():
         "img/foto5.jpg",
         "img/foto6.jpg"
     ]
+
     template_carrusel = env.get_template("carrusel.html")
-    html_carrusel = template_carrusel.render(imagenes=imagenes)
-    with open(os.path.join(output_dir, "carrusel.html"), "w", encoding="utf-8") as f:
-        f.write(html_carrusel)
-    print("✓ Carrusel generado: output/carrusel.html")
+    html_final = template_carrusel.render(imagenes=imagenes)
+
+    output_html_path = os.path.join(output_dir, "carrusel.html")
+    with open(output_html_path, "w", encoding="utf-8") as f:
+        f.write(html_final)
+    
+    print("Pagina generada correctamente")
+
+    shutil.copy(os.path.join(base_dir, "templates/styles.css"), output_dir)
+
+    img_src = os.path.join(base_dir, "img")
+    img_dst = os.path.join(output_dir, "img")
+    if os.path.exists(img_dst):
+        shutil.rmtree(img_dst)
+    shutil.copytree(img_src, img_dst)
+
+    logo_src = os.path.join(base_dir, "logo")
+    logo_dst = os.path.join(output_dir, "logo")
+    if os.path.exists(logo_dst):
+        shutil.rmtree(logo_dst)
+    shutil.copytree(logo_src, logo_dst)
+
 
     # --- Template con JSON ---
     template_datos = env.get_template("template.html")
