c927a91b21ab566c92931f09830944a53db3f67c
fp25ifcm0182 <fp25ifcm0182@aulatomillo.org>
2025-12-02 11:33:07 +0100

    carrusel nuevo

--- geoso2-web-template/modify_template.py
+++ geoso2-web-template/modify_template.py
@@ -4,10 +4,27 @@ Script para modificar un template HTML usando Jinja2
 """
 
 import json
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader, Template
 from datetime import datetime
+import os
 
 def main():
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(base_dir, "templates")
+    output_dir = os.path.join(base_dir, "output")
+    os.makedirs(output_dir, exist_ok=True)
+
+    os.makedirs("output", exist_ok=True)
+
+    env = Environment(loader=FileSystemLoader("templates"))
+
+    imagenes = [ "img/foto1.jpg", "img/foto2.png", "img/foto3.png", "img/foto4.jpg", "img/foto5.jpg", "img/foto6.jpg"]
+    template_carrusel = env.get_template("carrusel.html")
+    html_carrusel = template_carrusel.render(imagenes=imagenes)
+    with open("output/carrusel.html", "w", encoding="utf-8") as f:
+        f.write(html_carrusel)
+    print("Carrusel generado: output/carrusel.html")
     # Leer el template
     with open('template.html', 'r', encoding='utf-8') as file:
         template_content = file.read()
